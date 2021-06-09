9ca02d63f5863871d15db1041a7147583d60fc0a
Mohammad Eusha <eusha37@gmail.com>
2021-06-09 16:15:49 +0600

    first commit

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    style: {
+        postcss: {
+            plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
+    },
+}
